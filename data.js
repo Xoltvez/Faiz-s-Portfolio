f47b521dc064e3bb546057b17aa3a8af04178f85
Xoltvez <faizazzahra88@gmail.com>
2026-07-11 01:43:55 +0700

    Add New Portfolio

--- data.js
+++ data.js
@@ -43,8 +43,7 @@ export const projectsData = [
     role: "UI/UX Designer",
     description:
       "Designing user interface and user experience for a clothing thrift marketplace application.",
-    image:
-      "https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?q=80&w=800&auto=format&fit=crop",
+    image: "thriftspace/thriftspace.png",
     mockupType: "phone",
     detailImages: [
       "https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?q=80&w=800&auto=format&fit=crop",
